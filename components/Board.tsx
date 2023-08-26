6e0675073aef1189c15533e5b936999a0797773d
oktar <oktarkara@gmail.com>
2023-08-26 23:07:14 +0300

    feat[WIP]:creating appwrite db,zustand

---
+++ components/Board.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import React, { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+function Board() {
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  return (
+    <h1>Dana</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>{/* rendering all the columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
+  );
+}
+
+export default Board;
